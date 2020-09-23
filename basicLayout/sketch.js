0a63809ef66fddf2b7bbff0916aaac0c2487370f
pitworker <sir.button.esq@gmail.com>
2020-09-22 21:08:43 -0400

    added name sign in

--- basicLayout/sketch.js
+++ basicLayout/sketch.js
@@ -1,6 +1,9 @@
 const CONTINUOUS = true;
 const INTERIM_RESULTS = false;
 
+let signedIn;
+let name;
+
 let speechRec;
 
 // FUNCTIONS CALLED IN SETUP AND DRAW GO HERE
@@ -11,13 +14,41 @@ function gotSpeech(speech) {
     console.log('GOT SPEECH: ' + phrase);
 }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+function signIn() {
+    name = document.getElementById("nameInput").value;
 
-  let speech = new p5.Speech();
-  speechRec = new p5.SpeechRec(gotSpeech);
-  speechRec.start(CONTINUOUS, INTERIM_RESULTS);
+    document.getElementById("nameInput").remove();
+    document.getElementById("enterButton").remove();
+
+    createCanvas(windowWidth, windowHeight);
+
+    let speech = new p5.Speech();
+    speechRec = new p5.SpeechRec(gotSpeech);
+    speechRec.start(CONTINUOUS, INTERIM_RESULTS);
+
+    console.log(name);
+    signedIn = true;
+}
+
+function setup() {
+    signedIn = false;
+
+    let nameInput = document.createElement("INPUT");
+    nameInput.setAttribute("type", "text");
+    nameInput.setAttribute("id", "nameInput");
+    nameInput.setAttribute("name", "nameInput");
+
+    let enterButton = document.createElement("BUTTON");
+    enterButton.setAttribute("id", "enterButton");
+    enterButton.setAttribute("onclick", "signIn()");
+    enterButton.appendChild(document.createTextNode("ENTER"));
+
+    document.body.appendChild(nameInput);
+    document.body.appendChild(enterButton);
 }
 
 function draw() {
+    if (signedIn) {
+        background(255,204,0);
+    }
 }

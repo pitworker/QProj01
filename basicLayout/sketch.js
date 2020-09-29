d6a85813615171ea66fbd4bf456989de08615800
pitworker <sir.button.esq@gmail.com>
2020-09-29 18:52:40 -0400

    converted tree to object and randomized progression of branches

--- basicLayout/sketch.js
+++ basicLayout/sketch.js
@@ -1,10 +1,17 @@
 const CONTINUOUS = true;
 const INTERIM_RESULTS = false;
+const HEIGHT_MULT = 0.7;
+const PROB_MULT = 0.9;
+const THETA = Math.PI * 0.25;
 
 let signedIn;
 let name;
 
 let speechRec;
+
+let angleMult;
+
+let tree;
 
 // FUNCTIONS CALLED IN SETUP AND DRAW GO HERE
 function gotSpeech(speech) {
@@ -12,6 +19,59 @@ function gotSpeech(speech) {
 
     //console.log is the same thing as print
     console.log('GOT SPEECH: ' + phrase);
+}
+
+function branch(a,h,p) {
+    let n = {
+        angle:  a,
+        length: h,
+        right:  null,
+        left:   null,
+        isLeaf: true
+    };
+
+    if (random() < p) {
+        n.right = branch(a + THETA,
+                         h * HEIGHT_MULT,
+                         p * PROB_MULT);
+        n.isLeaf = false;
+    }
+
+    if (random() < p) {
+        n.left = branch(a - THETA,
+                        h * HEIGHT_MULT,
+                        p * PROB_MULT);
+        n.isLeaf = false;
+    }
+
+    return n;
+}
+
+function drawLeaf(r) {
+    noStroke();
+    fill(100,0,255);
+    circle(0, 0, r * 0.75);
+}
+
+function drawTree(b) {
+    console.log('drawTree called!');
+    if (b != null) {
+        push();
+
+        rotate(b.angle * angleMult);
+        strokeWeight(b.length / 2);
+        line(0, 0, 0, -b.length);
+        translate(0, -b.length);
+
+        if (b.isLeaf) {
+            drawLeaf(b.length);
+        } else {
+            drawTree(b.left);
+            drawTree(b.right);
+        }
+
+        pop();
+    }
 }
 
 function signIn() {
@@ -25,6 +85,10 @@ function signIn() {
     let speech = new p5.Speech();
     speechRec = new p5.SpeechRec(gotSpeech);
     speechRec.start(CONTINUOUS, INTERIM_RESULTS);
+
+    tree = branch(0,120,1);
+
+    console.log(tree);
 
     console.log(name);
     signedIn = true;
@@ -50,5 +114,14 @@ function setup() {
 function draw() {
     if (signedIn) {
         background(255,204,0);
+
+        angleMult = (mouseX / width);
+        // Start the tree from the bottom of the screen
+        translate(width/2,height);
+        // Draw a line 120 pixels
+        strokeWeight(50);
+        stroke(255);
+
+        drawTree(tree);
     }
 }
